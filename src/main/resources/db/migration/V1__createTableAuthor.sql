f596c738ed22cb980bccc505a909a2e65886e016
Rui Nascimento <sruy19@gmail.com>
2023-08-30 09:16:27 -0300

    refactor: files was formatted

--- src/main/resources/db/migration/V1__createTableAuthor.sql
+++ src/main/resources/db/migration/V1__createTableAuthor.sql
@@ -1,7 +1,8 @@
-CREATE TABLE authors (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL,
-    description TEXT(400) NOT NULL,
-    registred_at DATE NOT NULL
+CREATE TABLE authors
+(
+    id           BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name         VARCHAR(50)         NOT NULL,
+    email        VARCHAR(100) UNIQUE NOT NULL,
+    description  TEXT(400) NOT NULL,
+    registred_at DATE                NOT NULL
 );
--- src/main/resources/db/migration/V4__createTableBook.sql
+++ src/main/resources/db/migration/V4__createTableBook.sql
@@ -1,14 +1,15 @@
-CREATE TABLE books (
-    id               BIGINT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    title            VARCHAR(255) UNIQUE NOT NULL,
-    summary          TEXT NOT NULL,
+CREATE TABLE books
+(
+    id                BIGINT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    title             VARCHAR(255) UNIQUE NOT NULL,
+    summary           TEXT                NOT NULL,
     table_of_contents TEXT,
-    price            DECIMAL(10,2) NOT NULL,
-    number_of_pages  INT NOT NULL,
-    isbn             VARCHAR(13) UNIQUE NOT NULL,
-    publication_date DATE,
-    category_id      BIGINT NOT NULL,
-    author_id        BIGINT NOT NULL,
+    price             DECIMAL(10, 2)      NOT NULL,
+    number_of_pages   INT                 NOT NULL,
+    isbn              VARCHAR(13) UNIQUE  NOT NULL,
+    publication_date  DATE,
+    category_id       BIGINT              NOT NULL,
+    author_id         BIGINT              NOT NULL,
     FOREIGN KEY (category_id)
         REFERENCES categories (id),
     FOREIGN KEY (author_id)
