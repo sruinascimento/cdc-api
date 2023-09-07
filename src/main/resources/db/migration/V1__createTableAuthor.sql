ffa1f2eeec4747f4668dabe92d1366d20546ad22
Rui Nascimento <sruy19@gmail.com>
2023-09-07 16:51:43 -0300

    refact: change datatype from text to varchar

--- src/main/resources/db/migration/V1__createTableAuthor.sql
+++ src/main/resources/db/migration/V1__createTableAuthor.sql
@@ -3,6 +3,6 @@ CREATE TABLE authors
     id           BIGINT AUTO_INCREMENT PRIMARY KEY,
     name         VARCHAR(50)         NOT NULL,
     email        VARCHAR(100) UNIQUE NOT NULL,
-    description  TEXT(400) NOT NULL,
+    description  VARCHAR(400) NOT NULL,
     registred_at DATE                NOT NULL
 );
